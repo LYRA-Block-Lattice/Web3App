cf4c305fa4fe4233174dd6341e79642e200fbc9a
wizd <rcvbuf@gmail.com>
2022-12-29 16:42:07 +0800

    better order view

--- src/pages/ViewOrdersForm.tsx
+++ src/pages/ViewOrdersForm.tsx
@@ -21,56 +21,256 @@ const ViewOrdersForm: FunctionComponent = () => {
         </button>
       </div>
       <div className="ordercard3">
-        <button className="order-image">
-          <img
-            className="icbaseline-generating-tokens-icon"
-            alt=""
-            src="../asserts/icbaselinegeneratingtokens.svg"
-          />
-          <img
-            className="order-image-child"
-            alt=""
-            src="../asserts/arrow-1.svg"
-          />
-          <img
-            className="icbaseline-generating-tokens-icon"
-            alt=""
-            src="../asserts/carbonuserservicedesk.svg"
-          />
-        </button>
-        <div className="a-big-seller">A Big Seller</div>
-        <div className="price3">Price:</div>
-        <div className="amount9">Amount:</div>
-        <div className="a-big-buyer">A Big Buyer</div>
-        <div className="pricelabel">1,323</div>
-        <div className="amountlabel">3.2</div>
-        <img className="ordercard-child" alt="" src="../asserts/arrow-2.svg" />
+        <div className="frame-parent">
+          <button className="order-banner-wrapper">
+            <div className="order-banner">
+              <button className="order-image">
+                <img
+                  className="icbaseline-generating-tokens-icon"
+                  alt=""
+                  src="../asserts/icbaselinegeneratingtokens.svg"
+                />
+                <img
+                  className="order-image-child"
+                  alt=""
+                  src="../asserts/arrow-1.svg"
+                />
+                <img
+                  className="icbaseline-generating-tokens-icon"
+                  alt=""
+                  src="../asserts/carbonuserservicedesk.svg"
+                />
+              </button>
+              <div className="order-status">
+                <b className="open3">Open</b>
+              </div>
+            </div>
+          </button>
+          <div className="frame-group">
+            <div className="btc-parent">
+              <b className="btc">BTC</b>
+              <img
+                className="frame-child"
+                alt=""
+                src="../asserts/arrow-2.svg"
+              />
+              <b className="tetherusdt">tether/USDT</b>
+            </div>
+            <div className="btc">Price: 1,323</div>
+            <div className="btc">Amount: 3.2</div>
+          </div>
+        </div>
+        <div className="trade-table">
+          <div className="table">
+            <div className="row">
+              <div className="cell">
+                <div className="content">
+                  <div className="text">Buyer</div>
+                </div>
+              </div>
+              <div className="cell1">
+                <div className="content1">
+                  <div className="text1">Amount</div>
+                </div>
+              </div>
+              <div className="cell">
+                <div className="content">
+                  <div className="text">Time</div>
+                </div>
+              </div>
+              <div className="cell">
+                <div className="content1">
+                  <div className="text1">Status</div>
+                </div>
+              </div>
+              <div className="cell">
+                <div className="content1">
+                  <div className="text1">Action</div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="buyer-name-cell">
+                <div className="content1">
+                  <div className="text5">someone</div>
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content1">
+                  <div className="text6">1.0</div>
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content7">
+                  <div className="text5">3 hours ago</div>
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content1">
+                  <div className="text5">Pending</div>
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content1">
+                  <div className="text9" />
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="buyer-name-cell">
+                <div className="content10">
+                  <div className="text6" />
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content10">
+                  <div className="text6" />
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content10">
+                  <div className="text6" />
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content10">
+                  <div className="text6" />
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content10">
+                  <div className="text6" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="ordercard3">
-        <button className="order-image">
-          <img
-            className="icbaseline-generating-tokens-icon"
-            alt=""
-            src="../asserts/icbaselinegeneratingtokens1.svg"
-          />
-          <img
-            className="order-image-child"
-            alt=""
-            src="../asserts/arrow-1.svg"
-          />
-          <img
-            className="icbaseline-generating-tokens-icon"
-            alt=""
-            src="../asserts/carbonuserservicedesk.svg"
-          />
-        </button>
-        <div className="a-big-seller">A Big Seller</div>
-        <div className="price3">Price:</div>
-        <div className="amount9">Amount:</div>
-        <div className="a-big-buyer">A Big Buyer</div>
-        <div className="pricelabel">1,323</div>
-        <div className="amountlabel">3.2</div>
-        <img className="ordercard-child" alt="" src="../asserts/arrow-2.svg" />
+        <div className="frame-parent">
+          <button className="order-banner-wrapper">
+            <div className="order-banner">
+              <button className="order-image">
+                <img
+                  className="icbaseline-generating-tokens-icon"
+                  alt=""
+                  src="../asserts/icbaselinegeneratingtokens1.svg"
+                />
+                <img
+                  className="order-image-child"
+                  alt=""
+                  src="../asserts/arrow-1.svg"
+                />
+                <img
+                  className="icbaseline-generating-tokens-icon"
+                  alt=""
+                  src="../asserts/carbonuserservicedesk.svg"
+                />
+              </button>
+              <div className="order-status">
+                <b className="open3">Open</b>
+              </div>
+            </div>
+          </button>
+          <div className="frame-group">
+            <div className="btc-parent">
+              <b className="btc">BTC</b>
+              <img
+                className="frame-child"
+                alt=""
+                src="../asserts/arrow-2.svg"
+              />
+              <b className="tetherusdt">tether/USDT</b>
+            </div>
+            <div className="btc">Price: 1,323</div>
+            <div className="btc">Amount: 3.2</div>
+          </div>
+        </div>
+        <div className="trade-table">
+          <div className="table">
+            <div className="row">
+              <div className="cell">
+                <div className="content">
+                  <div className="text">Buyer</div>
+                </div>
+              </div>
+              <div className="cell1">
+                <div className="content1">
+                  <div className="text1">Amount</div>
+                </div>
+              </div>
+              <div className="cell">
+                <div className="content">
+                  <div className="text">Time</div>
+                </div>
+              </div>
+              <div className="cell">
+                <div className="content1">
+                  <div className="text1">Status</div>
+                </div>
+              </div>
+              <div className="cell">
+                <div className="content1">
+                  <div className="text1">Action</div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="buyer-name-cell">
+                <div className="content1">
+                  <div className="text5">someone</div>
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content1">
+                  <div className="text6">1.0</div>
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content7">
+                  <div className="text5">3 hours ago</div>
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content1">
+                  <div className="text5">Pending</div>
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content1">
+                  <div className="text9" />
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="buyer-name-cell">
+                <div className="content10">
+                  <div className="text6" />
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content10">
+                  <div className="text6" />
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content10">
+                  <div className="text6" />
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content10">
+                  <div className="text6" />
+                </div>
+              </div>
+              <div className="buyer-name-cell">
+                <div className="content10">
+                  <div className="text6" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
