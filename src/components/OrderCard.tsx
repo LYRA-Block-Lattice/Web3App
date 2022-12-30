81f73c687a4066f8812856fdcfe053951b826714
wizd <rcvbuf@gmail.com>
2022-12-30 14:32:47 +0800

    update

--- src/components/OrderCard.tsx
+++ src/components/OrderCard.tsx
@@ -103,7 +103,9 @@ const OrderCard: FunctionComponent<OrderCardType> = ({
           </div>
         </div>
       </div>
-      <div className="trades-section" />
+      <div className="trades-section">
+        <div className="width-controller" />
+      </div>
     </div>
   );
 };
