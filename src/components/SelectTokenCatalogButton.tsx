f4ed39415113ee7226c9424f78b11ac742fd9669
wizd <rcvbuf@gmail.com>
2023-01-07 04:02:28 +0800

    UI improve

--- src/components/SelectTokenCatalogButton.tsx
+++ src/components/SelectTokenCatalogButton.tsx
@@ -16,7 +16,7 @@ const SelectTokenCatalogButton: FunctionComponent<
         alt=""
         src={icbaselineGeneratingToken}
       />
-      <div className="token-container">
+      <div className="token-wrapper">
         <b className="token1">{token}</b>
       </div>
     </button>
