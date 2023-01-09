b336f8c984a7d66181c7c0dc7711a8faa983eae2
wizd <rcvbuf@gmail.com>
2023-01-10 02:12:13 +0800

    search token ok

--- src/pages/SellTokenToToken.tsx
+++ src/pages/SellTokenToToken.tsx
@@ -1,43 +1,68 @@
-import { FunctionComponent, useState, useCallback } from "react";
+import { FunctionComponent, useCallback, useState, useEffect } from "react";
 import { Autocomplete, TextField } from "@mui/material";
 import GeneralPopup from "../components/GeneralPopup";
 import PortalPopup from "../components/PortalPopup";
+import { useSearchParams } from "react-router-dom";
 import PriceAndCollateralForm from "../components/PriceAndCollateralForm";
 import "./SellTokenToToken.css";
-import CreateNFTForm from "./CreateNFTForm";
+import SearchTokenInput from "../dup/SearchTokenInput";
+
+interface customWindow extends Window {
+  rrComponent?: any;
+  rrProxy?: any;
+}
+declare const window: customWindow;
+
+interface IToken {
+  token: string;
+  domain: string;
+  isTOT: boolean;
+  name: string;
+}
 
 const SellTokenToToken: FunctionComponent = () => {
+  //const [isDisabled, setDisabled] = useState<boolean>(false);
+  const [searchParams, setSearchParams] = useSearchParams({});
+  const catsell = decodeURIComponent(searchParams.get("sell")!);
+  const catget = decodeURIComponent(searchParams.get("get")!);
+
+  const [tosell, setTosell] = useState("LYR");
+  const [toget, setToget] = useState("tether/USDT");
   const [isGeneralPopupOpen, setGeneralPopupOpen] = useState(false);
+
+  const [val, setVal] = useState<IToken>();
+
+  const onSellChange = useCallback((value) => {
+    console.log("onSellChange: " + value);
+    if (value) {
+      setTosell(value);
+    } else {
+      setTosell("");
+    }
+  }, [tosell]);
 
   const openGeneralPopup = useCallback(() => {
     setGeneralPopupOpen(true);
   }, []);
 
-  const closeGeneralPopup = useCallback(() => {
+  const closeGeneralPopup = useCallback((ticker) => {
+    if (ticker == null) {
+      console.log("popup closed. nothing happened.");
+    }
+    else {
+      console.log("popup closed!, the ticker is " + ticker);
+      setTosell(ticker);
+      setVal({ "token": ticker, "name": ticker } as IToken);
+    }
+
     setGeneralPopupOpen(false);
-  }, []);
+  }, [tosell, val]);
 
   return (
     <>
       <div className="selltokentotoken">
         <div className="searchtokenbyname2">
-          <div className="to-sell-token">To Sell Token</div>
-          <Autocomplete
-            sx={{ width: 301 }}
-            disablePortal
-            options={["aaa", "bbb", "ccc"]}
-            renderInput={(params: any) => (
-              <TextField
-                {...params}
-                color="primary"
-                label="Token Name"
-                variant="outlined"
-                placeholder=""
-                helperText=""
-              />
-            )}
-            size="medium"
-          />
+          <SearchTokenInput key="tosell" val={val} dir="Sell" cat={catsell} ownOnly={true} onTokenSelect={onSellChange} />
           <button
             className="prepare-sell-order-button11"
             onClick={openGeneralPopup}
@@ -45,34 +70,17 @@ const SellTokenToToken: FunctionComponent = () => {
             <div className="utility-button4">Mint to sell</div>
           </button>
           <div className="searchtokenbyname-child" />
-          <div className="to-sell-token">To Get Fiat</div>
-          <Autocomplete
-            sx={{ width: 301 }}
-            disablePortal
-            options={["aaa", "bbb", "ccc"]}
-            renderInput={(params: any) => (
-              <TextField
-                {...params}
-                color="primary"
-                label="Token Name"
-                variant="outlined"
-                placeholder=""
-                helperText=""
-              />
-            )}
-            size="medium"
-          />
+          <SearchTokenInput key="toget" val={val} dir="Get" cat={catget} ownOnly={false} onTokenSelect={setToget} />
         </div>
-        <PriceAndCollateralForm offering="offering" biding="biding" />
+        <PriceAndCollateralForm offering={tosell} biding={toget} />
       </div>
       {isGeneralPopupOpen && (
         <PortalPopup
           overlayColor="rgba(113, 113, 113, 0.3)"
           placement="Centered"
-          onOutsideClick={closeGeneralPopup}
+          onOutsideClick={() => closeGeneralPopup(null)}
         >
-          <GeneralPopup onClose={closeGeneralPopup}>
-            <CreateNFTForm />
+          <GeneralPopup tag={catsell} onClose={closeGeneralPopup}>
           </GeneralPopup>
         </PortalPopup>
       )}
