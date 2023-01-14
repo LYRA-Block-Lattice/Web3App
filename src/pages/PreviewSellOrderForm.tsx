166e112c88434b9fce428c1eb339dc425ee107b0
wizd <rcvbuf@gmail.com>
2023-01-14 15:00:34 +0800

    update

--- src/pages/PreviewSellOrderForm.tsx
+++ src/pages/PreviewSellOrderForm.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent, useCallback } from "react";
-import { FormControlLabel, Checkbox } from "@mui/material";
+import { TextField, FormControlLabel, Checkbox } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import "./PreviewSellOrderForm.css";
 
@@ -14,41 +14,82 @@ const PreviewSellOrderForm: FunctionComponent = () => {
     <div className="previewsellorderform">
       <div className="preview-sell-order">Preview Sell Order</div>
       <div className="im-selling">I’m selling:</div>
-      <div className="nft-name">
-        <div className="nft-name1">NFT Name</div>
-      </div>
+      <TextField
+        className="nft-name"
+        sx={{ width: 301 }}
+        color="primary"
+        variant="outlined"
+        type="text"
+        label="Token Name"
+        size="medium"
+        margin="none"
+        disabled
+      />
       <div className="im-selling">Buyer will pay me by:</div>
-      <div className="nft-name">
-        <div className="nft-name1">Token Name</div>
-      </div>
+      <TextField
+        className="nft-name"
+        sx={{ width: 301 }}
+        color="primary"
+        variant="outlined"
+        type="text"
+        label="Token Name"
+        size="medium"
+        margin="none"
+        disabled
+      />
       <div className="im-selling">Price, amount, and I’ll get:</div>
       <div className="nft-name-parent">
-        <div className="nft-name3">
-          <div className="price">Price</div>
-        </div>
+        <TextField
+          className="nft-name"
+          color="primary"
+          variant="outlined"
+          type="number"
+          label="Price"
+          size="medium"
+          margin="none"
+          disabled
+        />
         <div className="x">X</div>
-        <div className="nft-name3">
-          <div className="price">Amount</div>
-        </div>
+        <TextField
+          className="nft-name"
+          color="primary"
+          variant="outlined"
+          type="number"
+          label="Amount"
+          size="medium"
+          margin="none"
+          disabled
+        />
         <div className="x">=</div>
-        <div className="nft-name3">
-          <div className="price">Total</div>
-        </div>
+        <TextField
+          className="nft-name"
+          color="primary"
+          variant="outlined"
+          type="number"
+          label="Total"
+          size="medium"
+          margin="none"
+          disabled
+        />
       </div>
       <div className="im-selling">
         I’ll share the trade secret of bellow with buyer:
       </div>
-      <div className="tot-description4">
-        <div className="please-pay-to-container">
-          <p className="please-pay-to">Please pay to my bank account number:</p>
-          <p className="please-pay-to">&nbsp;</p>
-          <p className="please-pay-to">Bank of America</p>
-          <p className="p">1234 1234 1234 1234</p>
-        </div>
-      </div>
+      <TextField
+        sx={{ width: 301 }}
+        color="primary"
+        variant="outlined"
+        multiline
+        label={`Please pay to my bank account number:
+
+Bank of America
+1234 1234 1234 1234`}
+        margin="none"
+        disabled
+      />
       <div className="im-selling">The trade will be protected by:</div>
-      <div className="nft-name">
-        <div className="lyr-worth-10000">1,000,000 LYR, worth $10,000</div>
+      <div className="nft-name5">
+        <div className="x">1,000,000 LYR, worth $10,000</div>
       </div>
       <FormControlLabel
         className="confirm-before-create-order"
