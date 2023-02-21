a3a2b309f5fa4f3ceb2d48fc500136590b50032c
wizd <rcvbuf@gmail.com>
2023-02-21 15:24:27 +0800

    remove loading

--- src/pages/AssertDetailView.tsx
+++ src/pages/AssertDetailView.tsx
@@ -132,226 +132,220 @@ const AssertDetailView: FunctionComponent = () => {
 
   return (
     <div>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <div className="assertdetailview">
-          <PrimaryAccountCard />
-          <div className="assertdetailview1">
-            <div className="asserttitleregion">
-              <div className="assertauthorsection">
-                <div className="a-legend-nft">
-                  Author: {info?.Users.Author.UserName}
-                </div>
-                <div className="material-symbolsshare-parent">
-                  <img
-                    className="material-symbolsshare-icon"
-                    alt=""
-                    src="../asserts/materialsymbolsshare.svg"
-                  />
-                  <img
-                    className="material-symbolsshare-icon"
-                    alt=""
-                    src="../asserts/carbonoverflowmenuhorizontal.svg"
-                  />
-                </div>
+      <div className="assertdetailview">
+        <PrimaryAccountCard />
+        <div className="assertdetailview1">
+          <div className="asserttitleregion">
+            <div className="assertauthorsection">
+              <div className="a-legend-nft">
+                Author: {info?.Users.Author.UserName}
               </div>
-              <div className="asserttitlesection1">
-                <div className="meka-legends">
-                  {info?.Blocks.Offgen.Custom1 ?? info?.Blocks.Offgen.Ticker}
-                </div>
-              </div>
-              <div className="assertownersection">
-                <div className="meka-legends">
-                  Owner: {info?.Users.Seller.UserName}
-                </div>
+              <div className="material-symbolsshare-parent">
+                <img
+                  className="material-symbolsshare-icon"
+                  alt=""
+                  src="../asserts/materialsymbolsshare.svg"
+                />
+                <img
+                  className="material-symbolsshare-icon"
+                  alt=""
+                  src="../asserts/carbonoverflowmenuhorizontal.svg"
+                />
               </div>
             </div>
-            <div className="asserttitleregion">
-              <div className="icon-park-solidblockchain-wrapper">
-                <img
-                  className="material-symbolsshare-icon"
-                  alt=""
-                  src="../asserts/iconparksolidblockchain.svg"
-                />
+            <div className="asserttitlesection1">
+              <div className="meka-legends">
+                {info?.Blocks.Offgen.Custom1 ?? info?.Blocks.Offgen.Ticker}
               </div>
-              <img
-                className="titlebannerregion-child"
-                alt=""
-                src={info?.Meta?.image ?? "../asserts/frame-61@2x.png"}
-              />
             </div>
-            <div className="assertstatssection">
-              <div className="icoutline-remove-red-eye-parent">
-                <img
-                  className="material-symbolsshare-icon"
-                  alt=""
-                  src="../asserts/icoutlineremoveredeye.svg"
-                />
-                <div className="meka-legends">32 views</div>
-              </div>
-              <div className="icoutline-remove-red-eye-parent">
-                <img
-                  className="material-symbolsshare-icon"
-                  alt=""
-                  src="../asserts/mdicardsheartoutline.svg"
-                />
-                <div className="meka-legends">5 favorite</div>
-              </div>
-              <div className="icoutline-remove-red-eye-parent">
-                <img
-                  className="material-symbolsshare-icon"
-                  alt=""
-                  src="../asserts/mdishapeoutline.svg"
-                />
-                <div className="meka-legends">Fiat OTC</div>
+            <div className="assertownersection">
+              <div className="meka-legends">
+                Owner: {info?.Users.Seller.UserName}
               </div>
             </div>
           </div>
-          <div className="descriptiontitle">
-            <img
-              className="material-symbolsshare-icon"
-              alt=""
-              src="../asserts/fluenttextdescription20filled.svg"
-            />
-            <div className="meka-legends">Description</div>
-          </div>
-          <div className="descriptiondetails">
-            <div className="meka-legends">
-              {info?.Blocks.Offgen.Description ?? "[empty]"}
-            </div>
-          </div>
-          <div className="descriptiontitle">
-            <img
-              className="material-symbolsshare-icon"
-              alt=""
-              src="../asserts/icoutlinepricechange.svg"
-            />
-            <div className="meka-legends">Price and payment</div>
-          </div>
-          <div className="buysection">
-            <div className="priceandbuyregion">
-              <div className="pricelabel">
-                <div className="meka-legends">Current Price</div>
-              </div>
-              <div className="priceandvaluelabel">
-                <div className="meka-legends">
-                  {info?.Blocks.Order.Order.price}
-                </div>
-                <div className="tetherusdt-parent">
-                  <div className="tetherusdt">{info?.Blocks.Bidgen.Ticker}</div>
-                  <div className="div7">
-                    ${" "}
-                    {pricedollar.toLocaleString(undefined, {
-                      maximumFractionDigits: 2
-                    })}
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="setlimitsection">
-              <div className="pricelabel1">
-                <div className="meka-legends">Available to buy</div>
-              </div>
-              <div className="priceandvaluelabel1">
-                <div className="meka-legends">
-                  {info?.Blocks.Order.Order.limitMin} -{" "}
-                  {info?.Blocks.Order.Order.limitMax}
-                </div>
-                <div className="tetherusdt-parent">
-                  <div className="tetherusdt">{info?.Blocks.Offgen.Ticker}</div>
-                  <div className="div7">
-                    ${" "}
-                    {avalibleMinDollar.toLocaleString(undefined, {
-                      maximumFractionDigits: 2
-                    })}{" "}
-                    ~{" "}
-                    {avalibleMaxDollar.toLocaleString(undefined, {
-                      maximumFractionDigits: 2
-                    })}
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="pricelabel-parent">
-              <div className="pricelabel1">
-                <div className="meka-legends">My Offer</div>
-              </div>
-              <div className="frame-container">
-                <div className="selectedamount-parent">
-                  <input
-                    className="selectedamount"
-                    type="number"
-                    placeholder="150"
-                    defaultValue={info?.Blocks.Order.Order.limitMin! + ""}
-                    onChange={(e) => console.log(e)}
-                  />
-                  <div className="tethereth-group">
-                    <div className="tethereth1">
-                      {info?.Blocks.Offgen.Ticker}
-                    </div>
-                    <div className="tetherusdt">
-                      {bidAmount.toLocaleString(undefined, {
-                        maximumFractionDigits: 8
-                      })}{" "}
-                      {info?.Blocks.Bidgen.Ticker}
-                    </div>
-                    <div className="tetherusdt">
-                      ${" "}
-                      {offerDollar.toLocaleString(undefined, {
-                        maximumFractionDigits: 2
-                      })}
-                    </div>
-                  </div>
-                </div>
-                <div className="limitadjustsection">
-                  <div className="tetherusdt">Min</div>
-                  <Box className="slidercontinuous">
-                    <Slider
-                      color="primary"
-                      orientation="horizontal"
-                      step={10 ** (-1 * info?.Blocks.Offgen.Precision!)}
-                      min={info?.Blocks.Order.Order.limitMin}
-                      max={info?.Blocks.Order.Order.limitMax}
-                      onChange={(e, v) => setBuyAmount(v as number)}
-                      defaultValue={info?.Blocks.Order.Order.limitMin!}
-                    />
-                  </Box>
-                  <div className="max">Max</div>
-                </div>
-              </div>
-            </div>
-            <div className="makeofferbutton">
-              <button
-                className="prepare-sell-order-button5"
-                onClick={onMakeOfferButtonClick}
-              >
-                <img
-                  className="material-symbolsshare-icon"
-                  alt=""
-                  src="../asserts/evapricetagsoutline.svg"
-                />
-                <div className="primary-button2">Make offer</div>
-              </button>
-            </div>
-            <div className="descriptiontitle2">
+          <div className="asserttitleregion">
+            <div className="icon-park-solidblockchain-wrapper">
               <img
                 className="material-symbolsshare-icon"
                 alt=""
-                src="../asserts/galasecure.svg"
+                src="../asserts/iconparksolidblockchain.svg"
               />
-              <div className="meka-legends">Trading protection</div>
             </div>
-            <div className="descriptiondetails1">
-              <div className="a-legend-nft">
-                This order is protected by staking of 100,000 LYR, or $1,000 in
-                USD, from the seller. This order belongs to DAO "A good shop"
-                which has a total staking of 30,000,000 LYR.
-              </div>
+            <img
+              className="titlebannerregion-child"
+              alt=""
+              src={info?.Meta?.image ?? "../asserts/frame-61@2x.png"}
+            />
+          </div>
+          <div className="assertstatssection">
+            <div className="icoutline-remove-red-eye-parent">
+              <img
+                className="material-symbolsshare-icon"
+                alt=""
+                src="../asserts/icoutlineremoveredeye.svg"
+              />
+              <div className="meka-legends">32 views</div>
+            </div>
+            <div className="icoutline-remove-red-eye-parent">
+              <img
+                className="material-symbolsshare-icon"
+                alt=""
+                src="../asserts/mdicardsheartoutline.svg"
+              />
+              <div className="meka-legends">5 favorite</div>
+            </div>
+            <div className="icoutline-remove-red-eye-parent">
+              <img
+                className="material-symbolsshare-icon"
+                alt=""
+                src="../asserts/mdishapeoutline.svg"
+              />
+              <div className="meka-legends">Fiat OTC</div>
             </div>
           </div>
         </div>
-      )}
+        <div className="descriptiontitle">
+          <img
+            className="material-symbolsshare-icon"
+            alt=""
+            src="../asserts/fluenttextdescription20filled.svg"
+          />
+          <div className="meka-legends">Description</div>
+        </div>
+        <div className="descriptiondetails">
+          <div className="meka-legends">
+            {info?.Blocks.Offgen.Description ?? "[empty]"}
+          </div>
+        </div>
+        <div className="descriptiontitle">
+          <img
+            className="material-symbolsshare-icon"
+            alt=""
+            src="../asserts/icoutlinepricechange.svg"
+          />
+          <div className="meka-legends">Price and payment</div>
+        </div>
+        <div className="buysection">
+          <div className="priceandbuyregion">
+            <div className="pricelabel">
+              <div className="meka-legends">Current Price</div>
+            </div>
+            <div className="priceandvaluelabel">
+              <div className="meka-legends">
+                {info?.Blocks.Order.Order.price}
+              </div>
+              <div className="tetherusdt-parent">
+                <div className="tetherusdt">{info?.Blocks.Bidgen.Ticker}</div>
+                <div className="div7">
+                  ${" "}
+                  {pricedollar.toLocaleString(undefined, {
+                    maximumFractionDigits: 2
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="setlimitsection">
+            <div className="pricelabel1">
+              <div className="meka-legends">Available to buy</div>
+            </div>
+            <div className="priceandvaluelabel1">
+              <div className="meka-legends">
+                {info?.Blocks.Order.Order.limitMin} -{" "}
+                {info?.Blocks.Order.Order.limitMax}
+              </div>
+              <div className="tetherusdt-parent">
+                <div className="tetherusdt">{info?.Blocks.Offgen.Ticker}</div>
+                <div className="div7">
+                  ${" "}
+                  {avalibleMinDollar.toLocaleString(undefined, {
+                    maximumFractionDigits: 2
+                  })}{" "}
+                  ~{" "}
+                  {avalibleMaxDollar.toLocaleString(undefined, {
+                    maximumFractionDigits: 2
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="pricelabel-parent">
+            <div className="pricelabel1">
+              <div className="meka-legends">My Offer</div>
+            </div>
+            <div className="frame-container">
+              <div className="selectedamount-parent">
+                <input
+                  className="selectedamount"
+                  type="number"
+                  placeholder="150"
+                  defaultValue={info?.Blocks.Order.Order.limitMin! + ""}
+                  onChange={(e) => console.log(e)}
+                />
+                <div className="tethereth-group">
+                  <div className="tethereth1">{info?.Blocks.Offgen.Ticker}</div>
+                  <div className="tetherusdt">
+                    {bidAmount.toLocaleString(undefined, {
+                      maximumFractionDigits: 8
+                    })}{" "}
+                    {info?.Blocks.Bidgen.Ticker}
+                  </div>
+                  <div className="tetherusdt">
+                    ${" "}
+                    {offerDollar.toLocaleString(undefined, {
+                      maximumFractionDigits: 2
+                    })}
+                  </div>
+                </div>
+              </div>
+              <div className="limitadjustsection">
+                <div className="tetherusdt">Min</div>
+                <Box className="slidercontinuous">
+                  <Slider
+                    color="primary"
+                    orientation="horizontal"
+                    step={10 ** (-1 * info?.Blocks.Offgen.Precision!)}
+                    min={info?.Blocks.Order.Order.limitMin}
+                    max={info?.Blocks.Order.Order.limitMax}
+                    onChange={(e, v) => setBuyAmount(v as number)}
+                    defaultValue={info?.Blocks.Order.Order.limitMin!}
+                  />
+                </Box>
+                <div className="max">Max</div>
+              </div>
+            </div>
+          </div>
+          <div className="makeofferbutton">
+            <button
+              className="prepare-sell-order-button5"
+              onClick={onMakeOfferButtonClick}
+            >
+              <img
+                className="material-symbolsshare-icon"
+                alt=""
+                src="../asserts/evapricetagsoutline.svg"
+              />
+              <div className="primary-button2">Make offer</div>
+            </button>
+          </div>
+          <div className="descriptiontitle2">
+            <img
+              className="material-symbolsshare-icon"
+              alt=""
+              src="../asserts/galasecure.svg"
+            />
+            <div className="meka-legends">Trading protection</div>
+          </div>
+          <div className="descriptiondetails1">
+            <div className="a-legend-nft">
+              This order is protected by staking of 100,000 LYR, or $1,000 in
+              USD, from the seller. This order belongs to DAO "A good shop"
+              which has a total staking of 30,000,000 LYR.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
