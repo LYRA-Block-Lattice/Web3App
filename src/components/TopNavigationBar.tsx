e6c835e02483aa4e0b510aaba302e8356d8e2ab0
wizd <rcvbuf@gmail.com>
2023-02-22 19:06:55 +0800

    update

--- src/components/TopNavigationBar.tsx
+++ src/components/TopNavigationBar.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useCallback } from "react";
 import "./TopNavigationBar.css";
 
 type TopNavigationBarType = {
@@ -8,6 +8,18 @@ type TopNavigationBarType = {
 const TopNavigationBar: FunctionComponent<TopNavigationBarType> = ({
   title = "Lyra",
 }) => {
+  const onTitleClick = useCallback(() => {
+    //TODO: chroll main view to top
+  }, []);
+
+  const onHomeClick = useCallback(() => {
+    // Please sync "Home" to the project
+  }, []);
+
+  const onShareClick = useCallback(() => {
+    //TODO: popup context menu, share, etc.
+  }, []);
+
   return (
     <div className="navigation">
       <img
@@ -15,12 +27,26 @@ const TopNavigationBar: FunctionComponent<TopNavigationBarType> = ({
         alt=""
         src="../asserts/mini-programs-buttons.svg"
       />
-      <div className="title">{title}</div>
-      <img
-        className="mini-programs-buttons1"
-        alt=""
-        src="../asserts/mini-programs-buttons19.svg"
-      />
+      <button className="title" onClick={onTitleClick}>
+        {title}
+      </button>
+      <div className="mini-programs-buttons1">
+        <div className="stroke" />
+        <button className="home" onClick={onHomeClick}>
+          <img className="ellipse-icon" alt="" src="../asserts/ellipse.svg" />
+          <img className="ellipse-icon1" alt="" src="../asserts/ellipse1.svg" />
+        </button>
+        <button className="share" onClick={onShareClick}>
+          <img className="ellipse-icon2" alt="" src="../asserts/ellipse2.svg" />
+          <img className="ellipse-icon3" alt="" src="../asserts/ellipse3.svg" />
+          <img className="ellipse-icon4" alt="" src="../asserts/ellipse3.svg" />
+        </button>
+        <img
+          className="separator-icon"
+          alt=""
+          src="../asserts/separator9.svg"
+        />
+      </div>
     </div>
   );
 };
