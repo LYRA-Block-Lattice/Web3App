c75d176aa1d2ec54cdae254f8a50a6a5a6309dab
wizd <rcvbuf@gmail.com>
2023-02-28 19:54:06 +0800

    print fiat OK

--- src/components/MintFiatDialog.tsx
+++ src/components/MintFiatDialog.tsx
@@ -1,16 +1,90 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useCallback, useEffect, useState } from "react";
 import { Autocomplete, TextField } from "@mui/material";
 import "./MintFiatDialog.css";
-import { NeedRunTask } from "../app/utils";
+import { LongRunTask, NeedRunTask } from "../app/utils";
+import { useDispatch, useSelector } from "react-redux";
+import { getAppSelector } from "../app/selectors";
+import PrimaryButton from "./PrimaryButton";
+import { IBalance } from "../app/wallet/walletReducer";
+import { getWallet } from "../app/wallet/walletSaga";
 
 const MintFiatDialog: FunctionComponent<NeedRunTask> = (props) => {
+  const app = useSelector(getAppSelector);
+  const dispatch = useDispatch();
+  const [name, setName] = useState<string>("");
+  const [supply, setSupply] = useState<number>(0);
+  const [options, setOptions] = useState<IBalance[]>([]);
+
+  function getTokens() {
+    setOptions(app.wallet.balances.filter((a) => a.Domain == "fiat"));
+  }
+
+  useEffect(() => {
+    getTokens();
+  }, []);
+
+  const onMintClick = useCallback(() => {
+    console.log("printing fiat...");
+    const promises: LongRunTask[] = [
+      {
+        promise: (input) =>
+          new Promise(async (resolve, reject) => {
+            const wallet = getWallet();
+
+            const balanceResp = await wallet.printFiat(
+              input.ticker,
+              input.amount
+            );
+            if (balanceResp.resultCode == 0) {
+              resolve({ ...input, balanceResp: balanceResp });
+            } else {
+              reject(balanceResp.resultMessage);
+            }
+          }),
+        callback: null,
+        name: "Print Fiat",
+        description: "Send block to Lyra consensus network."
+      }
+    ];
+    if (props.onStart) {
+      const ret = props.onStart(
+        {
+          ticker: name,
+          amount: supply
+        },
+        promises
+      );
+      console.log(ret);
+    }
+    // dispatch({
+    //   type: WALLET_PRINT_FIAT,
+    //   payload: {
+    //     accountId: app.wallet.accountId,
+    //     ticker: name,
+    //     amount: supply
+    //   }
+    // });
+  }, [name, supply]);
+
+  const onGetTokenInputChange = useCallback(
+    (event: any, value: any, reason: any) => {
+      if (value) {
+        setName(value);
+      } else {
+        setName("");
+      }
+    },
+    [options]
+  );
   return (
     <div className="mintfiatdialog">
       <div className="print-fiat-for1">Print Fiat for free</div>
       <Autocomplete
         sx={{ width: 301 }}
         disablePortal
-        options={[] as any}
+        options={options}
+        onInputChange={onGetTokenInputChange}
+        getOptionLabel={(option) => option.Ticker}
         renderInput={(params: any) => (
           <TextField
             {...params}
@@ -33,10 +107,9 @@ const MintFiatDialog: FunctionComponent<NeedRunTask> = (props) => {
         placeholder="100"
         size="medium"
         margin="none"
+        onChange={(e) => setSupply(+e.target.value)}
       />
-      <button className="prepare-sell-order-button18">
-        <div className="secondary-button5">Print</div>
-      </button>
+      <PrimaryButton onClick={onMintClick}>Print</PrimaryButton>
     </div>
   );
 };
